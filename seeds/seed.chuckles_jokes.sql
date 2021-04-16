08024a96187ec4ec3e9abbd6c46f8e24c9113896
CaitlinGreer <clento1121@gmail.com>
2021-04-16 13:18:51 -0400

    initial seed file

---
+++ seeds/seed.chuckles_jokes.sql
@@ -0,0 +1,6 @@
+INSERT INTO chuckles_jokes (joke, punchline, submitted_by, date_submitted)
+VALUES
+    ('What kind of bees make milk?', 'BOO-Bees', 'Dumb Kid', '2021-01-20'),
+    ('What do you call a fly without wings?', 'A Walk', 'John Smith', '2021-03-23'),
+    ('What does a clock do when it"s hungry?', 'It goes back for seconds', 'Doc Brown', '2021-03-11'),
+    ('Why did Adele cross the road?', 'To say hello from the other side!', 'Lucy Jones', '2021-01-20');
