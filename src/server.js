2bf9f101b18b8b47d91ef28072840e2985c43674
CaitlinGreer <clento1121@gmail.com>
2021-04-21 12:39:42 -0400

    reverted SSL

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const { PORT, DATABASE_URL, SSL } = require('./config')
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: SSL 
 })
 
 app.set('db', db)
