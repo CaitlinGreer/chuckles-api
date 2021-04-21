60d2b5fa0ee19efa75cc5d7ccfc98fef0e05a5ae
CaitlinGreer <clento1121@gmail.com>
2021-04-21 13:46:23 -0400

    revert attempted changes

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,8 +1,5 @@
 require('dotenv').config();
 
-const pg = require('pg');
-pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
-
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
