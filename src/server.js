11ffabe4d933086d9e0cee4e730b9d5b2d28951e
CaitlinGreer <clento1121@gmail.com>
2021-04-23 11:56:20 -0400

    removed ssl in attempted migrate fix

--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: DATABASE_URL,
-    ssl: { 
-      rejectUnauthorized: false 
-    }
+    // ssl: { 
+    //   rejectUnauthorized: false 
+    // }
   }
 })
 
