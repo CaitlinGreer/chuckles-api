b0b70d9663429a1f4e3cdd9dc1386d752c265196
CaitlinGreer <clento1121@gmail.com>
2021-04-23 11:46:32 -0400

    troubleshooting migration issue

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const knex = require('knex')
 const app = require('./app')
-const { PORT, DATABASE_URL, SSL } = require('./config')
+const { PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
   client: 'pg',
   connection: {
     connectionString: DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: { 
+      rejectUnauthorized: false 
+    }
   }
 })
 
